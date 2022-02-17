d21ee47f10dbe51a42fa2a1653ae7b1b17d3e39a
Marcos-Correia <marcos91959@hotmail.com>
2022-02-17 14:49:27 -0300

    recipe-book list

--- src/app/recipe-book/list/list.component.ts
+++ src/app/recipe-book/list/list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-book-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
+  recipes:Recipe[] = [
+    new Recipe("Cake","Delicius chocolate cake","https://www.recipetineats.com/wp-content/uploads/2018/03/Chocolate-Cake_2.jpg?resize=650,910"),
+    // new Recipe("Cake","Delicius chocolate cake","https://www.recipetineats.com/wp-content/uploads/2018/03/Chocolate-Cake_2.jpg?resize=650,910")
+
+  ]
 
   constructor() { }
 
