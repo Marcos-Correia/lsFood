79899bb70f2978497f7a3da42793d592c11421d7
Marcos-Correia <marcos91959@hotmail.com>
2022-02-17 13:24:46 -0300

    create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ShoppingComponent } from './shopping/shopping.component';
+import { ListComponent } from './shopping/list/list.component';
+import { EditComponent } from './shopping/edit/edit.component';
+import { RecipeBookComponent } from './recipe-book/recipe-book.component';
+import { ItemComponent } from './recipe-book/item/item.component';
+import { DetailComponent } from './recipe-book/detail/detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShoppingComponent,
+    ListComponent,
+    EditComponent,
+    RecipeBookComponent,
+    ItemComponent,
+    DetailComponent
   ],
   imports: [
     BrowserModule
